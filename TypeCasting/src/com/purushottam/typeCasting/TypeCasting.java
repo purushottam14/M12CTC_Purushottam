72110da61527e11f766b13862542953fdad1e22c
#PRS# <#PRS#@Purushottam>
2022-03-17 15:25:04 +0530

    Type Casting

---
+++ TypeCasting/src/com/purushottam/typeCasting/TypeCasting.java
@@ -0,0 +1,29 @@
+package com.purushottam.typeCasting;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		
+	       float f = 10.532f;
+	       long l = (long) f;
+	       System.out.println("Casting Flaot to Long :" +l);
+	       System.out.println("==========================");
+	       int a=1200;
+	       double b=(double)a;
+	       System.out.println("Casting int to double :" +b);
+	       System.out.println("==========================");
+	       short x=1200;
+	        int y=(int)x;
+	       System.out.println("Casting short to int :" +y);
+	       System.out.println("==========================");
+	       byte o=120;
+	        int r=(int)o;
+	        System.out.println("Casting Byte to int :" +r);
+	       System.out.println("==========================");
+	       long z=1203115l;
+	        int w=(int)z;
+	        System.out.println("Casting long to int :" +w);
+	       System.out.println("==========================");
+	       
+	}
+
+}
