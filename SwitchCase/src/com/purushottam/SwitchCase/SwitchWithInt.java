93a82a3de9d24cd56bb7f4c0f39c8af411122b21
#PRS# <#PRS#@Purushottam>
2022-03-17 15:23:55 +0530

    Switch Case

---
+++ SwitchCase/src/com/purushottam/SwitchCase/SwitchWithInt.java
@@ -0,0 +1,29 @@
+package com.purushottam.SwitchCase;
+
+
+public class SwitchWithInt {
+	public static void main(String[] args) 
+	{
+		int x = 10;
+	 
+		
+		switch (x) 
+		{
+		case 1: 
+			System.out.println("The value of x is 1");
+			break;
+		case 2:
+			System.out.println("The value of x is 2");
+			break;
+		case 3:
+			System.out.println("The value of x is 3");
+			break;
+		default:
+		System.out.println("The value of x is other than 1,2,3");
+			break;
+
+		}
+
+	}
+
+}
---
+++ SwitchCase/src/com/purushottam/SwitchCase/SwitchWithString.java
@@ -0,0 +1,25 @@
+package com.purushottam.SwitchCase;
+
+public class SwitchWithString {
+	public static void main(String[] args)
+	{
+		String name = "team";
+
+		switch (name.toLowerCase()) 
+		{
+		case "author":
+			System.out.println("Vikas");
+			break;
+		case "team":
+			System.out.println("Team Java Full Stack");
+			break;
+		case "editor":
+			System.out.println("Vishnu & Krishna");
+			break;
+		default:
+			System.out.println("Invalid entry");
+			break;
+	}
+}
+
+}
