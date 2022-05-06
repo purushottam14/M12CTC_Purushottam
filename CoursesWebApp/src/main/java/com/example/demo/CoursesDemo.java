0914c7c7c3bed6b77bf0920dd5e3a043043f932e
#Purushottam# <#Purushottam#@Purushottam>
2022-05-06 20:50:16 +0530

    spring 3

---
+++ CoursesWebApp/src/main/java/com/example/demo/CoursesDemo.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Controller
+public class CoursesDemo {
+	@RequestMapping("/courses")
+	public String courses() {
+		return "courses1.jsp";
+	}
+}
